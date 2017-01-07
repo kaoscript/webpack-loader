6382d328f452be406d8a955dde884f3d5169f318
Baptiste Augrain <daiyam@zokugun.org>
2017-01-07 21:19:18 +0100

    support `register` parameter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * index.js
- * Version 0.2.0
+ * Version 0.3.0
  * December 29th, 2016
  *
  * Copyright (c) 2016 Baptiste Augrain
@@ -12,13 +12,21 @@ var fs = require('./fs.js');
 var util = require('loader-utils');
 
 var Compiler = _.Compiler;
-var defaultTarget = parseInt(/^v(\d+)\./.exec(process.version)[1]) >= 6 ? 'es6' : 'es5';
+var defaultTarget = parseInt(/^v(\d+)\./.exec(process.version)[1]) >= 6 ? 'ecma-v6' : 'ecma-v5';
 
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
 	
 	var filename = util.getRemainingRequest(this);
 	var params = util.parseQuery(this.query);
+	console.log(params)
+	
+	if(params.register) {
+		var modules = params.register.split('+');
+		for(var i = 0; i < modules.length; i++) {
+			require(modules[i])(Compiler);
+		}
+	}
 	
 	var target = params.target || defaultTarget;
 	
